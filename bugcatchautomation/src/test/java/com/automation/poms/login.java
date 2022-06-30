c389fd9953cd2c5bd7a95b89756bcf45492ec3d6
Tareka Long <tareka035@revature.net>
2022-06-30 10:03:31 -0500

    first project

---
+++ bugcatchautomation/src/test/java/com/automation/poms/login.java
@@ -0,0 +1,18 @@
+package com.automation.poms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+// this class is going to be my Java version of my login homepage
+public class login {
+  
+    private WebDriver driver;
+
+    public WebElement usernameInput;
+    public WebElement passwordInput;
+    public WebElement loginButton;
+
+    public login(WebDriver driver){
+
+    }
+}
